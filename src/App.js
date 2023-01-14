665ad57f27071dc5d24e420afadc76267e779ba8
ho1225 <56968144+ho1225@users.noreply.github.com>
2023-01-14 17:04:49 +0800

    create frontend structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import memories from './images/memories.png'
 const App = () => {
     return (
-        <div>
-            <h1>
-                App
-            </h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="600" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing>
+                        <Grid item xs={12} sm={7}>
+                            123
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            1123
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+        </Container>
     )
 }
 
