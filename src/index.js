b2655e91cd22a8835ffc10796f75051779ae0dce
ho1225 <56968144+ho1225@users.noreply.github.com>
2023-01-29 16:52:21 +0800

    styling and start to build api and redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, componse } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
+
+const store = createStore()
 
 ReactDom.render(<App />, document.getElementById('root'));
